cdea66cf071d537b5aeafc3c224f1366413b055e
cosli <cosli@foxmail.com>
2016-08-02 22:23:53 +0800

    updated initfire.sh & updated to firekylin_0.12.6

--- initfire.sh
+++ initfire.sh
@@ -1,10 +1,14 @@
 #/bin/bash
+if [ -d /firekylin/node_modules_useless ]
+then
+    rm -Rf /firekylin/node_modules_useless
+fi
 if [ -d /firekylin/node_modules ]
 then
     mv /firekylin/node_modules /firekylin/node_modules_useless
-    mv /node_modules /firekylin/node_modules
+    cp /node_modules /firekylin/node_modules
     pm2 start /firekylin/pm2.json
 else
-    mv /node_modules /firekylin/node_modules
+    cp /node_modules /firekylin/node_modules
     pm2 start /firekylin/pm2.json
 fi
