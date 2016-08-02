ef899e984eb6f7f031c2facc7abb1938e4238a5c
cosli <cosli@foxmail.com>
2016-08-02 23:59:03 +0800

    fix bug0

--- initfire.sh
+++ initfire.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 if [ -d /firekylin/node_modules_useless ]
 then
-    rm -Rf /firekylin/node_modules_useless
+    rm -rf /firekylin/node_modules_useless
 fi
 if [ -d /firekylin/node_modules ]
 then
     mv /firekylin/node_modules /firekylin/node_modules_useless
-    cp /node_modules /firekylin/node_modules
+    cp -r /node_modules /firekylin/node_modules
     pm2 start /firekylin/pm2.json
 else
-    cp /node_modules /firekylin/node_modules
+    cp -r /node_modules /firekylin/node_modules
     pm2 start /firekylin/pm2.json
 fi
